2d9de55fc78ba13a864a42fff6d8b1f6402462cb
AylaDursun <dursun.a@northeastern.edu>
2023-09-29 12:15:33 -0400

    first day

--- src/models/calculator.model.spec.ts
+++ src/models/calculator.model.spec.ts
@@ -1,14 +1,99 @@
 
 import { CalculatorModel } from './calculator.model';
+import { NumericKeys } from './../enums/numeric-keys.enum';
 import { ICalculatorModel } from '../interfaces/calculator-model.interface';
-
+import { OperatorKeys } from '../enums/operator-keys.enum';
+import { ActionKeys } from '../enums/action-keys.enum';
 describe('CalculatorModel', (): void => {
+
+  let calculator: ICalculatorModel;
+
+  beforeEach((): void => {
+    calculator = new CalculatorModel();
+  });
 
   it('should contain a CalculatorModel class that implements ICalculatorModel', (): void => {
 
-    const calculator: ICalculatorModel = new CalculatorModel();
-
     expect(calculator).toBeDefined();
+
+  });
+
+  it('should have an empty display on init', (): void => {
+
+    // Act
+    const displayValue: string = calculator.display();
+
+    // Assert
+    expect(displayValue).toEqual('');
+
+  });
+
+  it('should display `1` when the `1` key is pressed', (): void => {
+
+    // Act
+    calculator.pressNumericKey(NumericKeys.ONE);
+    const displayValue: string = calculator.display();
+
+    // Assert
+    expect(displayValue).toEqual('1');
+
+  });
+
+  it('should display `2` when the `2` key is pressed', (): void => {
+
+    calculator.pressNumericKey(NumericKeys.TWO);
+    const displayValue: string = calculator.display();
+
+    expect(displayValue).toEqual('2');
+
+  });
+
+  it('should display `98` when the `9` key is pressed followed by the `8` key', (): void => {
+
+    calculator.pressNumericKey(NumericKeys.NINE);
+    calculator.pressNumericKey(NumericKeys.EIGHT);
+    const displayValue: string = calculator.display();
+  
+    expect(displayValue).toEqual('98');
+  
+  });
+
+  it('should display + when the + key is pressed', (): void => {
+    calculator.pressOperatorKey(OperatorKeys.PLUS);
+    const displayValue: string = calculator.display();
+
+    expect(displayValue).toEqual("+");
+
+  });
+
+  it('should display nothing when the clear key is pressed', (): void => {
+    calculator.pressOperatorKey(OperatorKeys.PLUS);
+    calculator.pressActionKey(ActionKeys.CLEAR);
+
+    const displayValue: string = calculator.display();
+
+    expect(displayValue).toEqual("");
+
+  });
+
+  it('should display . when the dot key is pressed', (): void => {
+    calculator.pressActionKey(ActionKeys.DOT);
+
+    const displayValue: string = calculator.display();
+
+    expect(displayValue).toEqual(".");
+
+  });
+
+  it('should display 5 when the equals key is pressed ans 2 + 3 has been inputted', (): void => {
+    calculator.pressNumericKey(NumericKeys.TWO);
+    calculator.pressOperatorKey(OperatorKeys.PLUS);
+    calculator.pressNumericKey(NumericKeys.THREE);
+    calculator.pressActionKey(ActionKeys.EQUALS);
+
+    const displayValue: string = calculator.display();
+
+    expect(displayValue).toEqual("5");
 
   });
 
--- src/models/calculator.model.ts
+++ src/models/calculator.model.ts
@@ -1,25 +1,39 @@
-
 import { ActionKeys } from '../enums/action-keys.enum';
-import { NumericKeys } from '../enums/numeric-keys.enum';
-import { OperatorKeys } from '../enums/operator-keys.enum';
 import { ICalculatorModel } from '../interfaces/calculator-model.interface';
+import { NumericKeys } from './../enums/numeric-keys.enum';
+import { OperatorKeys } from './../enums/operator-keys.enum';
 
 export class CalculatorModel implements ICalculatorModel {
 
+  private _buffer: string = '';
+
   public pressNumericKey(key: NumericKeys): void {
-    throw new Error('Method not implemented.');
+    this._buffer += key;
   }
 
   public pressOperatorKey(key: OperatorKeys): void {
-    throw new Error('Method not implemented.');
+    this._buffer += key;
   }
 
   public pressActionKey(key: ActionKeys): void {
-    throw new Error('Method not implemented.');
+    switch(key) { 
+      case ActionKeys.CLEAR: { 
+        this._buffer = '';
+        break; 
+      } 
+      case ActionKeys.DOT: { 
+         this._buffer += "."
+         break; 
+      } 
+      case ActionKeys.EQUALS: { 
+        const values: string[] = this._buffer.split()
+        break; 
+      } 
+   } 
   }
 
   public display(): string {
-    throw new Error('Method not implemented.');
+    return this._buffer;
   }
 
 }
